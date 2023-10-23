672ee3825317054826612f15b9c1613b8224b567
Mateusz Kopij <sagasu@gmail.com>
2023-10-23 22:32:57 +0100

    updating dependent package versions

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from PyPDF2 import PdfReader
 fileReader = PdfReader(open('foo.pdf', 'rb'))
 
 ttsEngine = pyttsx3.init()
+text = ""
 
 for i,page in enumerate(fileReader.pages):
-    text = fileReader.pages[i].extract_text().strip().replace('\n', ' ')
+    text += fileReader.pages[i].extract_text().strip().replace('\n', ' ')
 
+print(text)
+ttsEngine.save_to_file('hello', 'HelloSound.mp3')
 ttsEngine.save_to_file(text, 'foo.mp3')
 ttsEngine.runAndWait()
+print('done')
 
 ttsEngine.stop()
